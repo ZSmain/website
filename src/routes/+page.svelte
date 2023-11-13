283676895f2c40695e591173dc76a0f51d14ced2
ismail <ishmael.zouaoui@gmail.com>
2023-11-13 21:31:13 +0100

    Add navbar and blog page and update the layout.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,6 @@
-<h1 class="text-3xl font-bold underline">Hola Mundo!</h1>
+<div class="container mx-auto">
+    <h1 class="text-3xl font-bold underline">Hola Mundo!</h1>
 
-<style lang="postcss">
-	:global(html) {
-		background-color: theme(colors.gray.100);
-	}
-</style>
+    <!-- this site is under construction -->
+    <div class="text-xl font-bold mt-4">This site is under construction</div>
+</div>
---
+++ src/routes/blog/+page.svelte
@@ -0,0 +1,3 @@
+<div class="container mx-auto">
+    <div>Blog</div>
+</div>
