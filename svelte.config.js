18fb493774f44973c50e78f39ff7d981d44db311
Ismail <ishmael.zouaoui@gmail.com>
2024-11-11 23:30:12 +0100

    feat: Add mdsvex and update dependencies

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,19 @@
+import { mdsvex } from 'mdsvex';
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: vitePreprocess(),
+	preprocess: [vitePreprocess(), mdsvex()],
 
 	kit: {
 		adapter: adapter(),
 		paths: {
 			base: process.env.NODE_ENV === 'production' ? '/website' : ''
 		}
-	}
+	},
+
+	extensions: ['.svelte', '.svx']
 };
 
 export default config;
